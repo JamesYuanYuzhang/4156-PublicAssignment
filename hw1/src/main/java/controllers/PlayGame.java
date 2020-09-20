daed6727626124b706f951f8065f5e1c5d2c700a
JamesYuanYuzhang <jamesyuanyuzhang@gmail.com>
2020-09-20 22:10:41 +0800

    hw1-1

--- hw1/src/main/java/controllers/PlayGame.java
+++ hw1/src/main/java/controllers/PlayGame.java
@@ -1,21 +1,37 @@
 package controllers;
 
+import com.google.gson.Gson;
 import io.javalin.Javalin;
 import java.io.IOException;
 import java.util.Queue;
+import models.GameBoard;
+import models.Message;
+import models.Move;
+import models.Player;
 import org.eclipse.jetty.websocket.api.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 class PlayGame {
 
   private static final int PORT_NUMBER = 8080;
 
   private static Javalin app;
+  
+  //The gameBoard for current game.
+  private static GameBoard gameBoard;
+  
+  //Transform object to JSON file and vice versa.
+  private static Gson gson = new Gson();
+  
+  //The logger to record exception information.
+  private static final Logger logger = LoggerFactory.getLogger(PlayGame.class);
 
   /** Main method of the application.
    * @param args Command line arguments
    */
   public static void main(final String[] args) {
-
     app = Javalin.create(config -> {
       config.addStaticFiles("/public");
     }).start(PORT_NUMBER);
@@ -24,34 +40,44 @@ class PlayGame {
     app.post("/echo", ctx -> {
       ctx.result(ctx.body());
     });
-
-    /**
-     * Please add your end points here.
-     * 
-     * 
-     * 
-     * 
-     * Please add your end points here.
-     * 
-     * 
-     * 
-     * 
-     * Please add your end points here.
-     * 
-     * 
-     * 
-     * 
-     * Please add your end points here.
-     * 
-     */
+    
+    // New game
+    app.get("/newgame", ctx -> {
+      gameBoard = new GameBoard();
+      ctx.redirect("tictactoe.html");
+    });
+    
+    //Start game
+    app.post("/startgame", ctx -> {
+      char type = (ctx.body().charAt(5));
+      gameBoard.setPlayer1(new Player(type, 1));
+      ctx.result(gson.toJson(gameBoard));
+    });
+    
+    //Another player join the game.
+    app.get("/joingame", ctx -> {
+      gameBoard.joinGame();
+      ctx.redirect("/tictactoe.html?p=2");
+      sendGameBoardToAllPlayers(gson.toJson(gameBoard));
+    });
+    
+    //Player ask to move.
+    app.post("/move/:playerId", ctx -> {
+      int i = Character.getNumericValue(ctx.body().charAt(2));
+      int j = Character.getNumericValue(ctx.body().charAt(6));
+      int playerId = Integer.valueOf(ctx.pathParam("playerId"));
+      Message message = gameBoard.move(i, j, playerId);
+      ctx.result(gson.toJson(message));
+      sendGameBoardToAllPlayers(gson.toJson(gameBoard));
+    });
 
     // Web sockets - DO NOT DELETE or CHANGE
     app.ws("/gameboard", new UiWebSocket());
   }
 
   /** Send message to all players.
-   * @param gameBoardJson Gameboard JSON
-   * @throws IOException Websocket message send IO Exception
+   * @param gameBoardJson gameBoard JSON
+   * @throws IOException webSocket message send IO Exception
    */
   private static void sendGameBoardToAllPlayers(final String gameBoardJson) {
     Queue<Session> sessions = UiWebSocket.getSessions();
@@ -60,6 +86,8 @@ class PlayGame {
         sessionPlayer.getRemote().sendString(gameBoardJson);
       } catch (IOException e) {
         // Add logger here
+        logger.error(e.getMessage());
+        e.printStackTrace();
       }
     }
   }
@@ -67,4 +95,5 @@ class PlayGame {
   public static void stop() {
     app.stop();
   }
+
 }
